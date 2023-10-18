d49cb74aea13d73278a08f5ecb35372f05c892f1
Ryan J <19141558@brookes.ac.uk>
2023-10-18 19:45:41 +0100

    Initial commit
    --to be completed--
    assertion statements
    logging load factor
    other logs (maybe log method)

--- StaffHash.java
+++ StaffHash.java
@@ -1,10 +1,12 @@
 package comp5017.cw1.pkg2023;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class StaffHash implements IStaffDB {
 
     private Employee[] table; // The hash table
     private int size; // size of table
-
     private static final int initialCapacity = 16; // the initial capacity of the hash table
     private static final double loadFactorThreshold = 0.5; // load factor threshold
 
@@ -15,26 +17,24 @@ public class StaffHash implements IStaffDB {
 
     @Override
     public void clearDB() {
-        // for each employee of Employee object within the table
-        for(Employee employee : table){
-            // if the employee exists
-            if(employee != null){
-                // so print their details
-                System.out.println(employee.getName() + ": " + employee.getAffiliation());
-            }
-        }
+        Arrays.fill(table, null);
+        size = 0;
     }
+
 
     @Override
     public boolean containsName(String name) {
         // use index to find names in hash table
         int index = hash(name);
+        int attempt = 0;
         // while a name exists
         while(table[index] != null) {
             // if the name at index of table equals the name then true
             if(table[index].getName().equals(name)) {
                 return true;
             }
+            attempt++;
+            index = probe(index,attempt);
         }
         return false;
     }
@@ -42,8 +42,9 @@ public class StaffHash implements IStaffDB {
     // research more hash methods
     public int hash(String name){
         int hash = 0;
+        int prime = 31;
         for(int i = 0; i < name.length(); i++) {
-            hash = hash * hash % table.length;
+            hash = (hash * prime + name.charAt(i)) % table.length;
         }
         return hash;
     }
@@ -52,15 +53,18 @@ public class StaffHash implements IStaffDB {
     public Employee get(String name) {
         // the index is the hash of the name
         int index = hash(name);
+        int attempt = 0;
         // while a name exists
         while(table[index] != null){
             // if name exists return it
             if(table[index].getName().equals(name)){
                 return table[index];
             }
+            attempt++;
+            index = probe(index,attempt);
         }
         return null;
-    }//;
+    }
 
     @Override
     // current size of table
@@ -110,7 +114,8 @@ public class StaffHash implements IStaffDB {
                 // else employee not found and doesnt already exist or error = collision
             } else {
                 //implement the probing here - probe method needed
-
+                attempt++;
+                index = probe(index,attempt);
             }
         }
         return null;
@@ -119,21 +124,61 @@ public class StaffHash implements IStaffDB {
     private void resize() {
         // create a new array
         Employee[] tableTwo = new Employee[table.length * 2];
+
         // allow the new array to be used
         for(Employee employee : table) {
-            if( employee != null){
-
+            if(employee != null){
+                // add employee
+                int index = hash(employee.getName());
+                int attempt= 0;
+                // while array is not empty
+                while(tableTwo[index] != null){
+                    attempt++;
+                    index = probe(index, attempt);
+                }
             }
         }
+        table = tableTwo;
+    }
+
+    // a quadratic prope for collisions
+    private int probe(int index, int attempt) {
+        return (index + attempt * attempt) % table.length;
     }
 
     @Override
     public Employee remove(String name) {
+        int index = hash(name);
+        int attempt = 0;
+        double loadFactor = (double) (size + 1) / table.length;
+
+        //while a name exists and the attempt is within table length
+        while(table[index] != null && attempt < table.length) {
+            //if employee with same name exists
+            if(table[index].getName().equals(name)) {
+                // remove it
+                Employee removedEmployee = table[index];
+                table[index] = null;
+                size--;
+                return removedEmployee;
+            } else {
+                //collision situation
+                attempt++;
+                index = probe(index, attempt);
+            }
+        }
         return null;
     }
 
     @Override
     public void displayDB() {
-
+        // for each employee of Employee object in table
+        for(Employee employee : table) {
+            // if employee exists
+            if(employee != null) {
+                //print details
+                System.out.println(employee.getName() + ": " + employee.getAffiliation());
+            }
+        }
     }
 }
